e426e7d7421dce23b1df83a893d5aacd5bde29aa
purwowd <id.purwowd@gmail.com>
2020-10-21 13:56:21 +0700

    Fixed

--- src/jwt_auth/settings/devs.py
+++ src/jwt_auth/settings/devs.py
@@ -149,6 +149,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 
 AUTH_USER_MODEL = 'users.User'
